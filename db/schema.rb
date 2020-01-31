75b9438b1885ef5e8b571809cd07eaa97f66ad11
momotarou1090 <raurauji0123@gmail.com>
2020-01-31 15:35:13 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_01_31_025454) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
