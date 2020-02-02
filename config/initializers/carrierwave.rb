94f97450b9028086d32baaf253ce44741cbe1ddc
lil-ikeda <ikepo.japon@gmail.com>
2020-02-02 20:25:56 +0900

    デプロイテスト/cariierwave.rb、image_uploader.rbのif文復活

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  # if Rails.env.producition? # 本番環境:AWSのS3を使用
+  if Rails.env.producition? # 本番環境:AWSのS3を使用
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -14,8 +14,8 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = 'frema'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/frema'
-  # else
-  #   config.storage :file # 開発環境:public/uploades下に保存
-  #   config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  # end
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end
 end
