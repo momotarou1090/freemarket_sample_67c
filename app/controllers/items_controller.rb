2dc44c8cd28d568fb6b6e01716d8e1082efcea52
lil-ikeda <ikepo.japon@gmail.com>
2020-02-02 10:59:19 +0900

    [WIP]商品詳細ページ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,23 +10,25 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-
-    # if @item.save!
-    #   redirect_to root_path
-    # else
-    #   render :new
-    # end
+    @item = Item.create(item_params)
+    if @item.save!
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
 
   def show
-    # @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
+    # @item.image
+    @user = User.where(id: @item.seller_id)
   end
 
   private
   def item_params
-
-    params.require(:item).permit(:seller_id, :categories_id, :name, :description, :postage, :region, :shipping_date, :price, :condition, :status, images_attributes: [:id, :image])
+    params.require(:item).permit(
+      :seller_id, :categories_id, :name, :description, :postage, :region, :shipping_date, :price, :condition, :status, images_attributes: [:id, :image]
+    )
   end
 
 end
