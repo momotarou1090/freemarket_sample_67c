fdf11723419301d1f396c606ceaa8247c3991f5d
holy2943 <59240738+holy2943@users.noreply.github.com>
2020-02-04 16:53:27 +0900

    [WIP]マークアップ仮完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items, only: [:index, :new, :create, :show]
   resources :images, only: [:create]
+  resources :users, only: [:show]
+  
 end
