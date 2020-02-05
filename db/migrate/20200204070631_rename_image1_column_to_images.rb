35c6f30be390abfa371cf34c30ff8d8ed568e59d
lil-ikeda <ikepo.japon@gmail.com>
2020-02-05 12:42:32 +0900

    本番環境migration_file修正02

--- db/migrate/20200204070631_rename_image1_column_to_images.rb
+++ db/migrate/20200204070631_rename_image1_column_to_images.rb
@@ -1,6 +1,6 @@
 class RenameImage1ColumnToImages < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :images, :image1, :image_one
+  # def change
+  #   rename_column :images, :image1, :image_one
 
-  end
+  # end
 end
