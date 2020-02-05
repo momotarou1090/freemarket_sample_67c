3ba28a1b0d0d7a2e6dca35fce5c7e3d51439bfc0
holy2943 <59240738+holy2943@users.noreply.github.com>
2020-02-05 13:12:02 +0900

    [WIP]マイグレーションファイルのコメントアウト

--- db/migrate/20200203103418_remove_status_from_images.rb
+++ db/migrate/20200203103418_remove_status_from_images.rb
@@ -1,5 +1,5 @@
 class RemoveStatusFromImages < ActiveRecord::Migration[5.2]
   def change
-    remove_column :images, :status, :text
+    # remove_column :images, :status, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_02_04_081535) do
-
-ActiveRecord::Schema.define(version: 2020_02_04_072926) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post_code", null: false
@@ -31,7 +28,6 @@ ActiveRecord::Schema.define(version: 2020_02_04_072926) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -90,7 +86,9 @@ ActiveRecord::Schema.define(version: 2020_02_04_072926) do
     t.string "phone_number", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "users"
