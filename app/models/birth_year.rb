f251731809787c3a6fc0728633277ede0f7be512
fumiya-ozaki <fumiya0414@gmail.com>
2020-02-16 11:40:50 +0900

    ユーザー新規登録フォーム修正

---
+++ app/models/birth_year.rb
@@ -0,0 +1,26 @@
+class BirthYear < ActiveHash::Base
+  self.data = [
+      {id: 0, name: ''},
+      {id: 1900, name: '1900'}, 
+      {id: 1901, name: '1901'}, 
+      {id: 1902, name: '1902'}, 
+      {id: 1903, name: '1903'}, 
+      {id: 1904, name: '1904'}, 
+      {id: 1905, name: '1905'}, 
+      {id: 1906, name: '1906'}, 
+      {id: 1907, name: '1907'}, 
+      {id: 1908, name: '1908'}, 
+      {id: 1909, name: '1909'}, 
+      {id: 1910, name: '1910'}, 
+      {id: 1911, name: '1911'}, 
+      {id: 1912, name: '1912'}, 
+      {id: 1913, name: '1913'}, 
+      {id: 1914, name: '1914'}, 
+      {id: 1915, name: '1915'}, 
+      {id: 1916, name: '1916'}, 
+      {id: 1917, name: '1917'}, 
+      {id: 1918, name: '1918'}, 
+      {id: 1919, name: '1919'}, 
+      {id: 1920, name: '1920'}, 
+  ]
+end
