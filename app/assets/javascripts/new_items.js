56d550a8e6337a4814eaad3e0a42e0095c1c9a59
Naohito Ikeda <ikepo.japon@gmail.com>
2020-02-17 19:10:42 +0900

    写真投稿フォームをJqueryを使ったものに改修

---
+++ app/assets/javascripts/new_items.js
@@ -0,0 +1,80 @@
+$(function(){
+  //DataTransferオブジェクトで、データを格納する箱を作る
+  var dataBox = new DataTransfer();
+  //querySelectorでfile_fieldを取得
+  var file_field = document.querySelector('input[type=file]')
+  //fileが選択された時に発火するイベント
+  $('#img-file').change(function(){
+    //選択したfileのオブジェクトをpropで取得
+    var files = $('input[type="file"]').prop('files')[0];
+    $.each(this.files, function(i, file){
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+
+      //DataTransferオブジェクトに対して、fileを追加
+      dataBox.items.add(file)
+      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
+      file_field.files = dataBox.files
+
+      var num = $('.item-image').length + 1 + i
+      fileReader.readAsDataURL(file);
+       //画像が10枚になったら超えたらドロップボックスを削除する
+      if (num == 4){
+        $('#image-box__container').css('display', 'none')   
+      }
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<div class='item-image' data-image="${file.name}">
+                    <div class=' item-image__content'>
+                      <div class='item-image__content--icon'>
+                        <img src=${src} width="160" height="170" >
+                      </div>
+                    </div>
+                    <div class='item-image__operetion'>
+                      <div class='item-image__operetion--delete'>削除</div>
+                    </div>
+                  </div>`
+        //image_box__container要素の前にhtmlを差し込む
+        $('#image-box__container').before(html);
+      };
+      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
+      $('#image-box__container').attr('class', `item-num-${num}`)
+    });
+  });
+  //削除ボタンをクリックすると発火するイベント
+  $(document).on("click", '.item-image__operetion--delete', function(){
+    //プレビュー要素を取得
+    var target_image = $(this).parent().parent()
+    //プレビューを削除
+    target_image.remove();
+    //inputタグに入ったファイルを削除
+    file_field.val("")
+  })
+});
+
+
+// $(document).on('turbolinks:load', ()=> {
+//   const buildFileField = (index)=> {
+//     const html = `<div data-index="${index}" class="js-file_group">
+//                     <input class="js-file" type="file"
+//                     name="product[images_attributes][${index}][src]"
+//                     id="product_images_attributes_${index}_src"><br>
+//                     <div class="js-remove">削除</div>
+//                     </div>`;
+//     return html;
+//   }
+
+//   let fileIndex = [1,2,3,4];
+
+//   $('#image-box').on('change', '.js-file', function(e) {
+//     $('#image-box').append(buildFileField(fileIndex[0]));
+//     fileIndex.shift();
+//     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+//   });
+
+//   $('#image-box').on('click', '.js-remove', function() {
+//     $(this).parent().remove();
+//     if ($('je-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+//   });
+// });
