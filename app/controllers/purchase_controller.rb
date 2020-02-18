a478d330d903094b9e3118745837f269cb87b2b0
fumiya-ozaki <fumiya0414@gmail.com>
2020-02-18 17:41:21 +0900

    購入済商品へのリンク修正
    URL直接入力でリンクできないよう設定

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -8,6 +8,12 @@ class PurchaseController < ApplicationController
     @image_first = @item.images[0]
     @image_others = @item.images[1..3]
     @address = Address.find_by(user_id: current_user)
+
+    if @item.status = "closed"
+      redirect_to item_path(@item),alert:"購入済商品です"
+    end
+
+
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     if card.blank?
       #登録された情報がない場合にカード登録画面に移動
@@ -23,6 +29,7 @@ class PurchaseController < ApplicationController
 
   def pay
     @item = Item.find(params[:item_id])
+      
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
